b420448775137dbca542b568e1484b3c69b9c4b9
wangqingcan <wangqingcan@baidu.com>
2017-04-05 15:08:25 +0800

    kNN

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -27,3 +27,39 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    # 取列的最大最小
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    # shape 0 为选行数
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet/tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+    
+
+
+
--- ch02/main.py
+++ ch02/main.py
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*-
+
 import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import array
 
 def main():
-    group, labels = kNN.createDataSet()
-    print kNN.classify0([0,0], group, labels, 3)
+    # group, labels = kNN.createDataSet()
+    # print kNN.classify0([0,0], group, labels, 3)
+    datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+    #print datingLabels
+    #print datingDataMat
+    # showData(datingDataMat, datingLabels)
+    normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+    print normMat
+    print ranges
+    print minVals
+
+def showData(datingDataMat, datingLabels):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # x,y color marker
+    ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15*array(datingLabels), 
+        15.0*array(datingLabels))
+    plt.show()
 
 main()
