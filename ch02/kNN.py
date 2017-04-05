2c62cb2fd375225492ce0019b3698b004b747860
wangqingcan <wangqingcan@baidu.com>
2017-04-05 23:09:27 +0800

    kNN

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -56,10 +56,29 @@ def autoNorm(dataSet):
     return normDataSet, ranges, minVals
 
 def datingClassTest():
-    hoRatio = 0.10
+    hoRatio = 0.1
     datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
-    datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-    
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # Y.shape is (n,m). So Y.shape[0] is n
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:],
+            datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is: %d" % \
+            (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
 
-
-
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr =  array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
+    print "You will probably like this person: ", resultList[classifierResult - 1] 
--- ch02/main.py
+++ ch02/main.py
@@ -8,14 +8,16 @@ from numpy import array
 def main():
     # group, labels = kNN.createDataSet()
     # print kNN.classify0([0,0], group, labels, 3)
-    datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-    #print datingLabels
-    #print datingDataMat
-    # showData(datingDataMat, datingLabels)
-    normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
-    print normMat
-    print ranges
-    print minVals
+    # datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+    # #print datingLabels
+    # #print datingDataMat
+    # # showData(datingDataMat, datingLabels)
+    # normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+    # print normMat
+    # print ranges
+    # print minVals
+    # kNN.datingClassTest()
+    kNN.classifyPerson()
 
 def showData(datingDataMat, datingLabels):
     fig = plt.figure()
