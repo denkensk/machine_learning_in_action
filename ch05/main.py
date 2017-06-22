bbfdf4efe27b6e21749c00080015840c71cfd984
denkensk <453102040@qq.com>
2017-06-23 01:37:07 +0800

    grad

---
+++ ch05/main.py
@@ -0,0 +1,7 @@
+import logRegres
+
+dataArr, labelMat = logRegres.loadDataSet()
+# weights = logRegres.gradAscent(dataArr, labelMat)
+# logRegres.plotBestFit(weights)
+weights = logRegres.stocGradAscent0(dataArr, labelMat)
+logRegres.plotBestFit(weights)
