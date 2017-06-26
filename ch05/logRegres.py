ca8867b7d88ef95611966b0f85a846c2ed8c7e24
denkensk <453102040@qq.com>
2017-06-26 23:23:12 +0800

    horse

--- ch05/logRegres.py
+++ ch05/logRegres.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import random
+import array
 
 
 def loadDataSet():
@@ -65,15 +67,79 @@ def plotBestFit(weights):
     plt.xlabel('x2')
     plt.show()
 
+
 def stocGradAscent0(dataMatrix, classLabels):
     m, n = np.shape(dataMatrix)
     alpha = 0.01
     weights = np.ones(n)
-    print weights
     for i in range(m):
         h = sigmoid(sum(dataMatrix[i] * weights))
         error = (classLabels[i] - h)
         print error
         weights = weights + alpha * error * np.array(dataMatrix[i])
     return weights
+
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = np.shape(dataMatrix)
+    weights = np.ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * np.array(dataMatrix[randIndex])
+            del (dataIndex[randIndex])
+    return weights
+
+
+def classifyVector(inX, weights):
+    print sum(inX * weights)
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+
+    trainingSet = []
+    trainingLabels = []
+
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+
+    trainWeights = stocGradAscent1(np.array(trainingSet), trainingLabels, 500)
+    errorCount = 0
+    numTestVec = 0.0
+
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+
+    errorRate = (float(errorCount) /numTestVec)
+    print "the error rate of this test is: %f" % errorRate
+    return errorRate
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(10):
+        errorSum += colicTest()
+    print "after %d iterations the average error rata is: %f" % (numTests, errorSum/float(numTests))
+
 
--- ch05/main.py
+++ ch05/main.py
@@ -1,7 +1,10 @@
 import logRegres
 
-dataArr, labelMat = logRegres.loadDataSet()
+# dataArr, labelMat = logRegres.loadDataSet()
 # weights = logRegres.gradAscent(dataArr, labelMat)
 # logRegres.plotBestFit(weights)
-weights = logRegres.stocGradAscent0(dataArr, labelMat)
-logRegres.plotBestFit(weights)
+# weights = logRegres.stocGradAscent0(dataArr, labelMat)
+# logRegres.plotBestFit(weights)
+# weights = logRegres.stocGradAscent1(dataArr, labelMat)
+# logRegres.plotBestFit(weights)
+logRegres.multiTest()
