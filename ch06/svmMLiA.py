b94b1e359756466f5b6f82ade1a5cb4cb001c30a
wangqingcan <wangqingcan@baidu.com>
2017-06-30 21:03:21 +0800

    stand svm

--- ch06/svmMLiA.py
+++ ch06/svmMLiA.py
@@ -97,3 +97,45 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
             iter = 0
         print "iteration number: %d" % iter
     return b, alphas
+
+
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m, 2)))
+
+    def calcEk(oS, k):
+        fXk = float(multiply(oS.alphas, oS.labelMat).T * (oS.X*oS.X[k, :].T)) + oS.b
+        Ek = fXk - float(oS.labelMat[k])
+        return Ek
+
+    def selectJ(self, i, oS, Ei):
+        maxK = -1
+        maxDeltaE = 0
+        Ej = 0
+        oS.eCache[i] = [1, Ei]
+        validEcacheList = nonzero(oS.eCache[:,0].A)[0]
+        if (len(validEcacheList)) > 1:
+            for k in validEcacheList:
+                if k == i:
+                    continue
+                Ek = self.calcEk(oS, k)
+                deltaE = abs(Ei - Ek)
+                if (deltaE > maxDeltaE):
+                    maxK = k
+                    maxDeltaE = deltaE
+                    Ej = Ek
+            return maxK, Ej
+        else:
+            j = selectJrand(i, oS.m)
+            Ej = self.calcEk(oS, j)
+        return j, Ej
+
+
+
